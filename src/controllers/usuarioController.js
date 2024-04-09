5e9188ccce232a5821567a87daca2e68f713b4eb
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-04-09 16:51:27 -0300

    função de status funcionando

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -20,15 +20,15 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                        usuarioModel.autenticar(email,senha)
+                            .then((resultadoAutenticar) => {
+                                if (resultadoAutenticar.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        senha: resultadoAutenticar[0].senha
+                               
                                     });
                                 } else {
                                     res.status(204).json({ aquarios: [] });

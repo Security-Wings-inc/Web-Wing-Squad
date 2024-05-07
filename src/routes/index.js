4c7a914f31d6a91cc4091e209ec03263e9695b9a
luca-sena <luca.souza@sptech.school>
2024-05-07 20:42:27 -0300

    index minusculo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("Index.html");
+    res.render("Index");
 });
 
 module.exports = router; 

694521f19d17f5a189728f800c381d853db9063f
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2024-05-10 01:15:13 -0300

    clean bugs of deletation garbage files from aquaTech

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var empresaRouter = require("./src/routes/empresa");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
 var usuarioRouter = require("./src/routes/usuario");
 var managerRouter = require("./src/routes/manager");
 const { Console } = require("console");
@@ -26,9 +23,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/empresa", empresaRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
 app.use("/usuario", usuarioRouter);
 app.use("/manager", managerRouter)
 
